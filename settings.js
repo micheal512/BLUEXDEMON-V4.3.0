0630c0fea6c7b96d6a4c0f6b842ce0ee81f29b41
micheal512 <76500745+micheal512@users.noreply.github.com>
2024-12-23 11:32:30 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,36 +1,36 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "234xxxxxxxxx"
+global.pairingNumber = "201070317780"
 
-global.botNumber = ["234xxxxxxxxx"]
+global.botNumber = ["201070317780"]
 
 global.anticall = false
 
 global.autoReadChat = false
 
-global.alwaysonline = true
+global.alwaysonline = false
 
 global.autoswview = false
 
-global.public = true
+global.public = false
 
 global.autoreact = false
 
-global.antidelete = false
+global.antidelete = true
 
 global.autotyping = false
 
-global.autoBio = true
+global.autoBio = false
 
 global.antiSpam = true
 
 global.sign = "〆"
 // ☞ ➻ ➽ ☛ 〆 
-global.bank = "Opay"
+global.bank = "Null"
 
-global.bankname = "BLUE"
+global.bankname = ""
 
-global.accnumber = "7041039367"
+global.accnumber = ""
 
 global.language = "en"
 global.sessionName = "session"
